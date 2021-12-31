8c3ae0f2a9eaac2ca328a92a6b2e1ca74fd76320
master1234791 <80994374+master1234791@users.noreply.github.com>
2021-12-30 20:01:16 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,16 +23,16 @@ function setup() {
   rope = new Rope(hero.body, { x: 400, y: 50 });
   monster = new Monster(1500,10,222);
 
-  box1 = new Box(900, 100, 70, 70);
-  box2 = new Box(900, 100, 70, 70);
-  box3 = new Box(900, 100, 70, 70);
-  box4 = new Box(900, 100, 70, 70);
-  box5 = new Box(900, 100, 70, 70);
-  box6 = new Box(800, 100, 70, 70);
-  box7 = new Box(800, 100, 70, 70);
-  box8 = new Box(800, 100, 70, 70);
-  box9 = new Box(800, 100, 70, 70);
-  box10 = new Box(800, 100, 70, 70);
+  box1 = new Box(900, 100, 50, 25);
+  box2 = new Box(900, 100, 50, 25);
+  box3 = new Box(900, 100, 50, 25);
+  box4 = new Box(900, 100, 50, 25);
+  box5 = new Box(900, 100, 50, 25);
+  box6 = new Box(800, 100, 50, 25);
+  box7 = new Box(800, 100, 50, 25);
+  box8 = new Box(800, 100, 50, 25);
+  box9 = new Box(800, 100, 50, 25);
+  box10 = new Box(800, 100, 50, 25);
 
 
 }
